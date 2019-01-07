21fe51eaa93de6c27cd42b5f619c00d517fc650e
Joshua Saucedo <joshuansu0897@gmail.com>
2019-01-07 11:47:31 -0700

    comenzamos

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>algo to React Native!</Text>
+        <Text style={styles.welcome}>Hola to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
